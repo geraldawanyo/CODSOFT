8e51e24ffea607c67924f56e6064c85914ee10c7
Gerald Awanyo <gerryawanyo@gmail.com>
2024-05-13 16:32:19 +0100

    new file:   .vs/CODSOFT/FileContentIndex/141e760e-3992-456a-a3e4-04ae77edc624.vsidx new file:   .vs/CODSOFT/FileContentIndex/943d6951-daec-46a6-bfb5-086f05bbf58f.vsidx new file:   .vs/CODSOFT/FileContentIndex/9f467f0d-8880-41df-896d-08bcd4c7d99d.vsidx new file:   .vs/CODSOFT/v17/.wsuo new file:   .vs/CODSOFT/v17/Browse.VC.db new file:   .vs/CODSOFT/v17/DocumentLayout.json new file:   .vs/CODSOFT/v17/ipch/AutoPCH/236cd37c9050333a/TASK_2.ipch new file:   .vs/CODSOFT/v17/ipch/AutoPCH/bfe693584301004c/TASK_3.ipch new file:   .vs/ProjectSettings.json new file:   .vs/VSWorkspaceState.json new file:   .vs/slnx.sqlite new file:   .vscode/launch.json new file:   .vscode/settings.json new file:   CppProperties.json new file:   task_3/task_3.cpp new file:   task_3/task_3.exe new file:   task_4/task_4.cpp new file:   task_4/task_4.exe

---
+++ task_3/task_3.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+
+using namespace std;
+
+
+char grid_position[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+const char playerX = 'X';
+const char playerO = 'O';
+char currentPlayer = playerX;
+char winner = ' ';
+
+
+
+
+
+
+int main() {
+    
+
+    for (int i = 0; i < 9; i++) {
+        cout << " " << grid_position[0] << " | " << grid_position[1] << " | " << grid_position[2] << '\n';
+        cout << "----" << "----" << "----" << '\n';
+        cout << " " << grid_position[3] << " | " << grid_position[4] << " | " << grid_position[5] << '\n';
+        cout << "----" << "----" << "----" << '\n';
+        cout << " " << grid_position[6] << " | " << grid_position[7] << " | " << grid_position[8] << '\n';
+
+        if (winner != ' ') {
+            break;
+        }
+       
+
+        int position;
+
+        cin >> position;
+
+        while (position < 1 || position > 9) {
+            cout << "Invalid Input - Try again" << endl;
+            cin >> position;
+
+            if (position > 0 && position < 10) {
+                break;
+
+            }
+
+        }
+        while (grid_position[position - 1] != ' ') {
+            cout << "Invalid Input - Try again" << endl;
+            cin >> position;
+
+            if (grid_position[position - 1] == ' ') {
+                break;
+
+            }
+        }
+
+        grid_position[position - 1] = currentPlayer;
+        currentPlayer = (currentPlayer == playerX) ? playerO : playerX;
+
+        for (int i = 0; i < 9; i += 3) {
+
+
+            if (grid_position[i] == grid_position[i + 1] && grid_position[i] == grid_position[i+2]) {
+
+                winner = grid_position[i];
+                break;
+
+            }
+
+        }
+
+        for (int i = 0; i < 3; i++) {
+
+            if (grid_position[i] == grid_position[i + 3] && grid_position[i] == grid_position[i + 6]) {
+
+                winner = grid_position[i];
+                break;
+            }
+
+        }
+
+        if (grid_position[0] == grid_position[4] && grid_position[0] == grid_position[8]) {
+
+            winner = grid_position[0];
+
+
+        }
+
+        else if (grid_position[2] == grid_position[4] && grid_position[2] == grid_position[6]) {
+
+            winner = grid_position[2];
+            
+
+        }
+
+    }
+
+    if (winner != ' ') {
+        cout << "Winner is Player" << winner << endl;
+
+    }
+    else {
+        cout << "Tie" << endl;
+    }
+
+    
+    
+
+
+
+
+
+
+}
+
